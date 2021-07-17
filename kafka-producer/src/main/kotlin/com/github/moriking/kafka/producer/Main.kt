c80df9d3d05adb4cb667dea453eb06e9bf0da8e1
morteza.neishaboori@gmail.com <morteza.neishaboori@gmail.com>
2021-07-17 19:22:11 +0300

    Changed project structure

--- kafka-producer/src/main/kotlin/com/github/moriking/kafka/producer/Main.kt
+++ kafka-producer/src/main/kotlin/com/github/moriking/kafka/producer/Main.kt
@@ -1,4 +1,4 @@
-package com.github.moriking
+package com.github.moriking.kafka.producer
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
