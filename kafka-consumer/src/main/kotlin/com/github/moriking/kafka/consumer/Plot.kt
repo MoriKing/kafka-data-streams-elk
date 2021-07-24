33d95e4e0f880ecb5ec6dc6bc0653c1ce4d55202
morteza.neishaboori@gmail.com <morteza.neishaboori@gmail.com>
2021-07-24 13:57:17 +0300

    Fixed plot builder

--- kafka-consumer/src/main/kotlin/com/github/moriking/kafka/consumer/Plot.kt
+++ kafka-consumer/src/main/kotlin/com/github/moriking/kafka/consumer/Plot.kt
@@ -5,27 +5,29 @@ import org.charts.dataviewer.api.config.DataViewerConfiguration
 import org.charts.dataviewer.api.data.PlotData
 import org.charts.dataviewer.api.trace.BarTrace
 
-class Plot(title: String, xTitle: String, yTitle: String) {
+class Plot(private val title: String, private val xTitle: String, private val yTitle: String) {
   private val dataViewer = DataViewer(title.replace(" ", ""))
   private val values = mutableMapOf<String, Long>("" to 0)
-
-  init {
-    with(DataViewerConfiguration()) {
-      plotTitle = title
-      setxAxisTitle(xTitle)
-      setyAxisTitle(yTitle)
-      showLegend(false)
-      dataViewer.updateConfiguration(this)
-    }
-  }
 
   fun updateValue(key: String, value: Long) {
     values[key] = value
   }
 
-  fun build() = with(BarTrace<Any>()) {
-    setxArray(values.keys.toTypedArray())
-    setyArray(values.values.toTypedArray())
-    dataViewer.updatePlot(PlotData(this))
+  fun build() {
+    dataViewer.resetPlot()
+    updateConfiguration()
+    with(BarTrace<Any>()) {
+      setxArray(values.keys.toTypedArray())
+      setyArray(values.values.toTypedArray())
+      dataViewer.updatePlot(PlotData(this))
+    }
+  }
+
+  private fun updateConfiguration() = with(DataViewerConfiguration()) {
+    plotTitle = title
+    setxAxisTitle(xTitle)
+    setyAxisTitle(yTitle)
+    showLegend(false)
+    dataViewer.updateConfiguration(this)
   }
 }
